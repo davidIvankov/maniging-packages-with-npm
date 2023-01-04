7e7249b12539163bf65c511e28661d75392cb4e8
davidIvankov <david.ivankov96@gmail.com>
2023-01-04 21:07:54 +0100

    feat: added css

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ let app = express();
 app.get("/", function(req, res){
 res.sendFile(__dirname + "/views/index.html")
 });
+app.use("/", express.static(__dirname + "/public"))
 //console.log("Hello World")
 
 
