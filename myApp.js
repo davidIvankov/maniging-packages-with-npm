5d975c51ee0dc70342d423d3ccb592a9985d70e8
davidIvankov <david.ivankov96@gmail.com>
2023-01-04 21:12:15 +0100

    fix: position

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
-
+app.use("/", express.static(__dirname + "/public/style.css"))
 app.get("/", function(req, res){
 res.sendFile(__dirname + "/views/index.html")
 });
-app.use("/", express.static(__dirname + "/public/style.css"))
+
 //console.log("Hello World")
 
 
