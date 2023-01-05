b548a4b9bef993e7ad31c5c9ab3599da8a69fc08
davidIvankov <david.ivankov96@gmail.com>
2023-01-05 13:38:03 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-app.use("/", express.static(__dirname + "/public/style.css"))
+app.use("/", express.static(__dirname + "/public"))
 app.get("/", function(req, res){
 res.sendFile(__dirname + "/views/index.html")
 });
