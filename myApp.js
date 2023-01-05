22edcaa541d1e8f93af8095b7b8e450030df8beb
davidIvankov <david.ivankov96@gmail.com>
2023-01-05 14:52:29 +0100

    feat: /:word/echo path

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ app.use(function middlewear(req, res, next){
     next()
 })
 app.use("/public", express.static(__dirname + "/public"))
+app.get("/:word/echo", function(req, res){
+    res.json({
+        word: req.params.word
+    })
+})
 app.get("/now", function(req, res, next){
     req.time = new Date().toString()
     next()
