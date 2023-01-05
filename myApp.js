ba7f48e13d43f01ba35a2bfd6c902d6e1b419814
davidIvankov <david.ivankov96@gmail.com>
2023-01-05 15:23:26 +0100

    feat: bodyParser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 require('dotenv').config()
+let bodyParser = require('body-parser')
 let app = express();
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(function middlewear(req, res, next){
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next()
