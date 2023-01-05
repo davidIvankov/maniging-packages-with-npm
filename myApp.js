b0e3e71da536239449437615e1bb8eab537a1dd6
davidIvankov <david.ivankov96@gmail.com>
2023-01-05 13:50:10 +0100

    feat: added json

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 app.use("/public", express.static(__dirname + "/public"))
+app.get("/json", function(req, res){
+    res.json({
+        message:"Hello json"
+    })
+})
 app.get("/", function(req, res){
 res.sendFile(__dirname + "/views/index.html")
 });
